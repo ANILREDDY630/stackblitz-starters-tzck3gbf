686b45e6571853608c9d3551de4bf1ccce75cada
anil reddy <lokaanilreddy5@gmail.com>
2025-04-01 11:39:41 +0530

    changes

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const CryptoJS = require('crypto-js');
+
+const secretKey = process.env.SECRET_KEY;
+
 const encrypt = (payload) => {
   // encrypt the payload and return token
+  const token = jwt.sign(payload, secretKey, {expiresIn: '1h'});
+
+  const encryptedToken = CryptoJS.AES.encrypt(token, secretKey).toString();
+  return encryptedToken;
 }
 
 const decrypt = (token) => {
   // return decoded payload
+  try{
+    const bytes = CryptoJS.AES.decrypt(token, secretKey);
+    const decryptedToken = bytes.toString(CryptoJS.enc.Utf8);
+    const decoded = jwt.verify(decryptedToken, secretKey);
+    return decoded;
+  } catch (error) {
+    console.error("Invalid token or decryption failed");
+    return null;
+  }
 }
 
 module.exports = {
